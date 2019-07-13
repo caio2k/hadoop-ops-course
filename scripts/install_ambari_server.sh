a374046d73d849fa5c237a123c262e9caf4a0991
caio2k <3915835+caio2k@users.noreply.github.com>
2019-07-13 21:42:22 +0200

    update hdp to 3.1

--- scripts/install_ambari_server.sh
+++ scripts/install_ambari_server.sh
@@ -3,7 +3,8 @@
 setenforce 0
 
 # Download Ambari Repository
-wget -nv http://public-repo-1.hortonworks.com/ambari/centos7/2.x/updates/2.2.0.0/ambari.repo -O /etc/yum.repos.d/ambari.repo
+#curl -fsSL http://public-repo-1.hortonworks.com/ambari/centos7/2.x/updates/2.2.0.0/ambari.repo -o /etc/yum.repos.d/ambari.repo
+curl -fsSL http://public-repo-1.hortonworks.com/ambari/centos7/2.x/updates/2.7.3.0/ambari.repo -o /etc/yum.repos.d/ambari.repo
 
 # Install java-1.8
 yum -y install java-1.8.0-openjdk java-1.8.0-openjdk-devel
